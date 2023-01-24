a0679da9a514763e80cc0ceedf101201db834b87
Louis Lachaize <114236521+louiiiiiis@users.noreply.github.com>
2023-01-24 01:12:07 +0100

    Add files via upload

--- my_test.go
+++ my_test.go
@@ -6,10 +6,9 @@ func square(x int) int {
 }
 
 func main() {
-	var x, y int;
-	x = 5;
-	y = 2;
-	fmt.Print(square(x) + 1, " ", y, " ", square(y), " ", square(y) + 1, "\n");
+	for n := 0; n <= 10; n++ {
+	fmt.Print("carré de ", n, " : ", square(n), "\n");
+	}
 }
 
 
